cf9c3f5ab0be97962c5e5d5d92d26fbb5711538c
Kevin Sullivan <KSullivan@nhl.com>
2020-07-25 16:33:47 -0400

    Add a React component

---
+++ src/compiled/Item/items.js
@@ -0,0 +1,38 @@
+var items = [{
+    image: 'MONITER',
+    name: 'Acer CB272',
+    description: 'Cheap Starting Moniter',
+    price: 100
+}, {
+    image: 'MONITER',
+    name: 'Samsung XD2400',
+    description: 'Slightly Better Moniter',
+    price: 109
+}, {
+    image: 'MONITER',
+    name: 'Asus RX42',
+    description: 'Good Moniter With Good Graphics',
+    price: 119
+}, {
+    image: 'MONITER',
+    name: 'Samsung XD2400',
+    description: 'Slightly Better Moniter',
+    price: 109
+}, {
+    image: 'MONITER',
+    name: 'HP CB324',
+    description: 'Really Good Garphics',
+    price: 139
+}, {
+    image: 'MONITER',
+    name: 'Samsung XD2400',
+    description: 'Slightly Better Moniter',
+    price: 109
+}, {
+    image: 'MONITER',
+    name: 'Acer UHD420',
+    description: 'Amazing Grahics Quality And 144hz Refresh Rate',
+    price: 169
+}];
+
+export default items;
