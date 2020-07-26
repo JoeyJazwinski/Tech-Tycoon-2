06c0617fd594b0af8ee9c5c78bbaa6160f798e5f
Kevin Sullivan <KSullivan@nhl.com>
2020-07-25 22:52:19 -0400

    Do we decide to do React?

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ShopModal from './components/ShopModal/ShopModal';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ShopModal />
+  </React.StrictMode>,
+  document.getElementById('shopGui')
+);
